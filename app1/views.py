5b9c3b4e4223c78bcb27120f23ca68dc822cf08d
Reis567 <matheusdosreis9@gmail.com>
2024-09-15 08:58:00 -0300

    👌 IMPROVE: data of teams

--- app1/views.py
+++ app1/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import redirect
-
+from django.db.models import Count, Q
 
 
 
 def home(request):
-    times = Time.objects.all()
-    partidas = Partida.objects.filter(status='FINALIZADA')
-    return render(request, 'home.html', {'times': times, 'partidas': partidas})
 
+    times = Time.objects.annotate(
+        num_jogadores=Count('jogador', filter=Q(jogador__isnull=False)), 
+        num_partidas=Count(
+            'time_casa', filter=Q(time_casa__status='FINALIZADA')
+        ) + Count(
+            'time_visitante', filter=Q(time_visitante__status='FINALIZADA')
+        )
+    )
+    
+    partidas = Partida.objects.filter(status='FINALIZADA')
+    
+    return render(request, 'home.html', {'times': times, 'partidas': partidas})
 
 def iniciar_transmissao(request):
     # Aqui você pode adicionar a lógica para iniciar a transmissão.
