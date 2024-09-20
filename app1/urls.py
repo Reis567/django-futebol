2a9c31e578274728a1f3307c758846e9d2656ffd
Reis567 <matheusdosreis9@gmail.com>
2024-09-20 07:08:47 -0300

    👌 IMPROVE: url team details

--- app1/urls.py
+++ app1/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('transmissao/<int:partida_id>/', views.transmissao_partida, name='transmissao_partida'),
     path('registrar_gol/<int:jogador_id>/<int:partida_id>/<str:tipo_gol>/', views.registrar_gol, name='registrar_gol'),
     path('remover_gol/<int:jogador_id>/<int:partida_id>/<str:tipo_gol>/', views.remover_gol, name='remover_gol'),
+    path('time/<int:time_id>/', views.time_detail, name='time_detail'),
 ]
