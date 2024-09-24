3ad306c2aff6c5a13aa68d46aff6a9d916dfbd06
Reis567 <matheusdosreis9@gmail.com>
2024-09-24 08:23:59 -0300

    👌 IMPROVE: urls

--- app1/urls.py
+++ app1/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('registrar_gol/<int:jogador_id>/<int:partida_id>/<str:tipo_gol>/', views.registrar_gol, name='registrar_gol'),
     path('remover_gol/<int:jogador_id>/<int:partida_id>/<str:tipo_gol>/', views.remover_gol, name='remover_gol'),
     path('time/<int:time_id>/', views.time_detail, name='time_detail'),
+    path('partida/<int:partida_id>/jogador/<int:jogador_id>/adicionar_cartao/', views.adicionar_cartao, name='adicionar_cartao'),
+    path('partida/<int:partida_id>/jogador/<int:jogador_id>/remover_cartao/', views.remover_cartao, name='remover_cartao'),
 ]
